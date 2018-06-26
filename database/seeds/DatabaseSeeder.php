3d243207cdfdf61bcd7abf7ce6c3866f22a7428e
Amir ur Rehman <Amir@Amir-2.local>
2018-06-26 08:12:03 +0400

    some modification in jWT

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Experience::class, 10)->create();
-        factory(App\ExperienceList::class, 100)->create();
-        factory(App\Project::class, 15)->create();
-        factory(App\Element::class, 40)->create();
-        factory(App\ProjectPage::class, 70)->create();
-        factory(App\ProjectElement::class, 90)->create();
-
+        // factory(App\Experience::class, 10)->create();
+        // factory(App\ExperienceList::class, 100)->create();
+        // factory(App\Project::class, 15)->create();
+        // factory(App\Element::class, 40)->create();
+        // factory(App\ProjectPage::class, 70)->create();
+        // factory(App\ProjectElement::class, 90)->create();
+        factory(App\User::class, 10)->create();
     }
 }
